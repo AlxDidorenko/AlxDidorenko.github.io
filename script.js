2ebf7cd686f4b598a48ffe67373fdff953fdeced
AlxDidorenko <47829900+AlxDidorenko@users.noreply.github.com>
2022-12-05 22:32:53 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const start = new Date();
-window.addEventListener('DOMContentLoaded', () => {
-    console.log(`DOM fully loaded and parsed; Time: ${Date.now() - start} ms`)
-})  
-window.addEventListener('load', () => {
-    console.log(`Page is fully loaded; Time: ${Date.now() - start} ms`)
-})
-  
